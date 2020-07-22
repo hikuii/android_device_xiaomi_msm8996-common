104b832dc7b939451f2f043caee23d93c195db5b
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    msm8996-common: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9
    
    Conflicts:
    	manifest.xml
    	msm8996.mk
    
    Ref: https://github.com/LineageOS/android_device_xiaomi_msm8998-common/commit/93d53cbba61e0eb8ef62def8b3374ce36aa80b18

--- msm8996.mk
+++ msm8996.mk
@@ -388,11 +388,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
