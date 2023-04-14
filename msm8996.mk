bc597881833ac6bc9f1ae9ba481411b26a5c9e3b
lee.wp14 <lee.wp14@gmail.com>
2023-04-14 16:09:21 +0800

    Revert "msm8996-common: Switch to source built vendor.qti.hardware.perf@1.0"
    This reverts commit a8056250a678f03aef6b18f9b341b36f842a7e3b.
    
    Conflicts:
    	msm8996.mk
    	proprietary-files.txt

--- msm8996.mk
+++ msm8996.mk
@@ -298,8 +298,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@1.0.vendor
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
