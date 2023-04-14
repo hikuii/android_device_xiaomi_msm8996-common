6c089bc525488c9fb558ae7c38f2dbf9cb8fecfd
lee.wp14 <lee.wp14@gmail.com>
2023-04-14 18:16:02 +0800

    msm8996-common: Remove QTI Power HAL
    This actually requires libqti-perfd-client to work.
    We are switching over to EAS, and libqti-perfd-client is being removed.

--- msm8996.mk
+++ msm8996.mk
@@ -296,10 +296,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
